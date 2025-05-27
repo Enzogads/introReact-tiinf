715f80558fd7dca3ec984c652888cf6f0ed73ff1
Enzogads <enzogadspc@gmail.com>
2025-05-26 22:22:01 -0300

    Feat: Introdução ao REACT

---
+++ ola/src/components/Body.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Body.css'
+
+function Body() {
+  return (
+    <div className='Body'>
+        <h2 className='titulo2'>Dinos são legais</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor quibusdam, esse vel rem delectus corporis sunt in assumenda. Ipsum consequuntur voluptatem officia. Unde ipsam iusto libero aliquid! Temporibus, unde aliquam.</p>
+    </div>
+  )
+}
+
+export default Body
