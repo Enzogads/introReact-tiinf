a414b37030b830785a86abbc89c4dfcbb9e31971
Enzogads <enzogadspc@gmail.com>
2025-05-27 22:14:42 -0300

    primeiro form react

--- ola/src/components/Body.jsx
+++ ola/src/components/Body.jsx
@@ -1,12 +1,77 @@
+import { useState } from 'react'
 import React from 'react'
 import './Body.css'
 
 function Body() {
+  const[cont, setCont] = useState(0)
+  const[inputValor, setInputValor] = useState('')
+  const[inputN1, setInputN1] = useState('')
+  const[inputN2, setInputN2] = useState('')
+
+function aumentar(){
+  setCont(cont +1)
+}
+
+function diminui(){
+  if(cont > 0){
+    setCont(cont -1)
+  }
+}
+
+function alterar(){
+  setCont(Number(inputValor))
+}
+
+function somar(){
+  setCont(Number(inputN1) + Number(inputN2))
+}
+
+function subtrair(){
+  setCont(Number(inputN1) - Number(inputN2))
+}
+
+function multiplicar(){
+  setCont(Number(inputN1) * Number(inputN2))
+}
+
+
   return (
     <div className='Body'>
         <h2 className='titulo2'>Dinos são legais</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor quibusdam, esse vel rem delectus corporis sunt in assumenda. Ipsum consequuntur voluptatem officia. Unde ipsam iusto libero aliquid! Temporibus, unde aliquam.</p>
+        <div className='cont'>
+        <button onClick={diminui}>-</button>
+        <p>Contagem: {cont}</p>
+        <button onClick={aumentar}>+</button>
+        </div>
+
+        <div className="form">
+          <input type="number" 
+            value={inputValor}
+            onChange={(e) => setInputValor(e.target.value)}
+          />
+          <button onClick={alterar}>Alterar</button>
+        </div>
+
+        <div className="calculadeira">
+          <input type="text" 
+          value={inputN1}
+          onChange={((e) => setInputN1(e.target.value))}
+          />
+
+          <button onClick={somar}>+</button>
+          <button onClick={subtrair}>-</button>
+          <button onClick={multiplicar}>*</button>
+
+          <input type="text"
+          value={inputN2}
+          onChange={((e) => setInputN2(e.target.value))}
+          />
+
+        </div>
+
     </div>
+
+    
   )
 }
 
